35d23fc7b7019e75cf9382f7e86037a1a4726af1
Ana luiza <analuibm@gmail.com>
2022-10-04 14:20:27 -0300

    adding filters

---
+++ src/pages/Menu/Filters/index.tsx
@@ -0,0 +1,31 @@
+import styles from './Filters.module.scss';
+import React from 'react';
+import filters from './filters.json';
+
+import classNames from 'classnames';
+
+type IOption = typeof filters[0];
+
+interface Props {
+  filter: number | null;
+  setFilter: React.Dispatch<React.SetStateAction<null | number>>
+}
+
+export default function Filters({ filter, setFilter }: Props) {
+  const selectFilter = (option: IOption) => {
+    return filter === option.id ? setFilter(null) : setFilter(option.id);
+  }
+
+  return (
+    <div className={styles.filters}>
+      {filters.map((option) => (
+        <button onClick={() => selectFilter(option)} className={classNames({
+          [styles.filters__filter]: true,
+          [styles['filters__filter--active']]: filter === option.id
+        })} key={option.id}>
+          {option.label}
+        </button>
+      ))}
+    </div>
+  )
+}
